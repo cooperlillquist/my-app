ec528363b5815b27d8f187500d2bf8e2e373cf4d
Cooper Lillquist <lillquist.1@osu.edu>
2024-01-10 16:04:49 -0500

    fixing eslintrc.js and index

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,19 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+      browser: true,
+      es2020: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 11,
+      sourceType: 'module'
     },
-    "plugins": [
-        "react"
-    ],
+    plugins: ['react'],
     "rules": {
-        "no-extra-semi" : "error"
+        "no-extra-semi":"error"
     }
-}
+  };
