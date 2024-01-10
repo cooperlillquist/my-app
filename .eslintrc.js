58f88a68f14cbd8635e6db62962c5134a6e5c455
Cooper Lillquist <lillquist.1@osu.edu>
2024-01-10 16:34:20 -0500

    eslint file creation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,32 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-      browser: true,
-      es2020: true
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 11,
-      sourceType: 'module'
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    plugins: ['react'],
+    "plugins": [
+        "react"
+    ],
     "rules": {
-        "no-extra-semi":"error"
     }
-  };
+}
